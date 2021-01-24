a399c4094681980980e3df6361eed327cea803fa
edanur çetin <edanurcetin16@hotmail.com>
2021-01-24 21:39:41 +0300

    Add project files.

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<TKey,TValue>
+    {
+
+        TKey[] keyArray;
+        TValue[] valueArray;
+
+        TKey[] tempKeyArray;
+        TValue[] tempValueArray;
+
+        public MyList()
+        {
+            keyArray = new TKey[0];
+            valueArray = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            tempKeyArray = keyArray;
+            tempValueArray = valueArray;
+
+            keyArray = new TKey[keyArray.Length + 1];
+            valueArray = new TValue[valueArray.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keyArray[i] = tempKeyArray[i];
+            }
+
+            for (int i = 0; i < valueArray.Length; i++)
+            {
+                valueArray[i] = tempValueArray[i];
+            }
+
+            keyArray[keyArray.Length - 1] = key;
+            valueArray[valueArray.Length - 1] = value;
+
+        }
+
+        public TKey[] items1
+        {
+            get { return keyArray; }
+        }
+
+        public TValue[] items2
+        {
+            get { return valueArray; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int, string> idProduct = new MyList<int, string>();
+            idProduct.Add(100, "Süt");
+            idProduct.Add(101, "Peynir");
+            idProduct.Add(102, "Sıvı Yağ");
+            idProduct.Add(103, "Makarna");
+            idProduct.Add(104, "Yoğurt");
+
+            Console.WriteLine("Ürün: " + idProduct.items1[0] + " " + "ID: " + idProduct.items2[0]);
+            Console.WriteLine("Ürün: " + idProduct.items1[1] + " " + "ID: " + idProduct.items2[1]);
+            Console.WriteLine("Ürün: " + idProduct.items1[2] + " " + "ID: " + idProduct.items2[2]);
+            Console.WriteLine("Ürün: " + idProduct.items1[3] + " " + "ID: " + idProduct.items2[3]);
+            Console.WriteLine("Ürün: " + idProduct.items1[4] + " " + "ID: " + idProduct.items2[4]);
+
+
+
+
+        }
+    }
+}
